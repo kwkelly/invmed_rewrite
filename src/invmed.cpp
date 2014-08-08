cd23a03a29ddd7b9d9f80652483e32d19fcce8e9
Keith Kelly <keith@ices.utexas.edu>
2014-08-08 16:36:45 -0500

    Adds matrix mult function, adds vec2tree, tree2vec functions. Adds KSP solve code

--- src/invmed.cpp
+++ src/invmed.cpp
@@ -3,8 +3,138 @@
 #include <petscksp.h>
 #include <profile.hpp>
 #include "funcs.hpp"
+//#include "petsc_utils.hpp"
+#include "typedefs.hpp"
 #include <pvfmm_common.hpp>
+//BEGIN COPY FROM UTILS
+struct InvMedData{
+	InvMedTree<FMM_Mat_t>* phi_0;
+	InvMedTree<FMM_Mat_t>* temp;
+};
 
+#undef __FUNCT__
+#define __FUNCT__ "mult"
+int mult(Mat M, Vec U, Vec Y);
+
+#undef __FUNCT__
+#define __FUNCT__ "tree2vec"
+template <class FMM_Mat_t>
+int tree2vec(InvMedTree<FMM_Mat_t> *tree, Vec& Y);
+
+#undef __FUNCT__
+#define __FUNCT__ "vec2tree"
+template <class FMM_Mat_t>
+int vec2tree(Vec& Y, InvMedTree<FMM_Mat_t> *tree);
+
+int mult(Mat M, Vec U, Vec Y){
+
+	PetscErrorCode ierr;
+	InvMedData *invmed_data = NULL;
+	MatShellGetContext(M, &invmed_data);
+	InvMedTree<FMM_Mat_t>* phi_0 = invmed_data->phi_0;
+	InvMedTree<FMM_Mat_t>* temp = invmed_data->temp;
+
+	const MPI_Comm* comm=invmed_data->phi_0->Comm();
+	int cheb_deg = InvMedTree<FMM_Mat_t>::cheb_deg;
+	int omp_p=omp_get_max_threads();
+	vec2tree(U,temp);
+
+	temp->Multiply(phi_0,1);
+
+	// Run FMM ( Compute: G[ \eta * u ] )
+	temp->ClearFMMData();
+	temp->RunFMM();
+	temp->Copy_FMMOutput();
+
+	// Regularize
+	tree2vec(temp,Y);
+
+	PetscScalar alpha = (PetscScalar).00001;
+	ierr = VecAXPY(Y,alpha,U);CHKERRQ(ierr);
+
+	return 0;
+}
+		
+#undef __FUNCT__
+#define __FUNCT__ "tree2vec"
+template <class FMM_Mat_t>
+int tree2vec(InvMedTree<FMM_Mat_t> *tree, Vec& Y){
+	PetscErrorCode ierr;
+	int cheb_deg=InvMedTree<FMM_Mat_t>::cheb_deg;
+
+	std::vector<FMMNode_t*> nlist = tree->GetNGLNodes();
+
+	int omp_p=omp_get_max_threads();
+	size_t n_coeff3=(cheb_deg+1)*(cheb_deg+2)*(cheb_deg+3)/6;
+
+	{
+		PetscInt Y_size;
+		ierr = VecGetLocalSize(Y, &Y_size);
+		int data_dof=Y_size/(n_coeff3*nlist.size());
+		int SCAL_EXP = 1;
+
+		PetscScalar *Y_ptr;
+		ierr = VecGetArray(Y, &Y_ptr);
+
+		#pragma omp parallel for
+		for(size_t tid=0;tid<omp_p;tid++){
+			size_t i_start=(nlist.size()* tid   )/omp_p;
+			size_t i_end  =(nlist.size()*(tid+1))/omp_p;
+			for(size_t i=i_start;i<i_end;i++){
+				pvfmm::Vector<double>& coeff_vec=nlist[i]->ChebData();
+				double s=std::pow(0.5,COORD_DIM*nlist[i]->Depth()*0.5*SCAL_EXP);
+
+				size_t Y_offset=i*n_coeff3*data_dof;
+				for(size_t j=0;j<n_coeff3*data_dof;j++) Y_ptr[j+Y_offset]=coeff_vec[j]*s;
+			}
+		}
+		ierr = VecRestoreArray(Y, &Y_ptr);
+	}
+
+	return 0;
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "vec2tree"
+template <class FMM_Mat_t>
+int vec2tree(Vec& Y, InvMedTree<FMM_Mat_t> *tree){
+	PetscErrorCode ierr;
+	const MPI_Comm* comm=tree->Comm();
+	int cheb_deg = InvMedTree<FMM_Mat_t>::cheb_deg;
+
+	std::vector<FMMNode_t*> nlist = tree->GetNGLNodes();
+
+	int omp_p=omp_get_max_threads();
+	size_t n_coeff3=(cheb_deg+1)*(cheb_deg+2)*(cheb_deg+3)/6;
+
+	{
+		PetscInt Y_size;
+		ierr = VecGetLocalSize(Y, &Y_size);
+		int data_dof=Y_size/(n_coeff3*nlist.size());
+		int SCAL_EXP = 1;
+
+		const PetscScalar *Y_ptr;
+		ierr = VecGetArrayRead(Y, &Y_ptr);
+
+		#pragma omp parallel for
+		for(size_t tid=0;tid<omp_p;tid++){
+			size_t i_start=(nlist.size()* tid   )/omp_p;
+			size_t i_end  =(nlist.size()*(tid+1))/omp_p;
+			for(size_t i=i_start;i<i_end;i++){
+				pvfmm::Vector<double>& coeff_vec=nlist[i]->ChebData();
+				double s=std::pow(2.0,COORD_DIM*nlist[i]->Depth()*0.5*SCAL_EXP);
+
+				size_t Y_offset=i*n_coeff3*data_dof;
+				for(size_t j=0;j<n_coeff3*data_dof;j++) coeff_vec[j]=PetscRealPart(Y_ptr[j+Y_offset])*s;
+				nlist[i]->DataDOF()=data_dof;
+			}
+		}
+	}
+
+	return 0;
+}
+
+//END COPY FROM UTILS
 #undef __FUNCT__
 #define __FUNCT__ "main"
 int main(int argc, char* argv[]){
@@ -61,15 +191,12 @@ int main(int argc, char* argv[]){
 
   PetscOptionsGetReal(NULL,       "-eta" ,&    eta_   ,NULL);
 
-	std::cout << "__PROFILE__: " << __PROFILE__ << std::endl;
-	#ifdef __VERBOSE__
-	std::cout << "__VERBOSE__: on" << std::endl;
-	#endif
+	pvfmm::Profile::Enable(true);
 
 	// Define some stuff!
+
 	typedef pvfmm::FMM_Node<pvfmm::Cheb_Node<double> > FMMNode_t;
 	typedef pvfmm::FMM_Cheb<FMMNode_t> FMM_Mat_t;
-
   const pvfmm::Kernel<double>* kernel=&pvfmm::ker_helmholtz;
   pvfmm::BoundaryType bndry=pvfmm::FreeSpace;
 
@@ -89,6 +216,12 @@ int main(int argc, char* argv[]){
 	pt_sources->kernel = kernel;
 	pt_sources->fn = pt_sources_fn;
 	pt_sources->f_max = 1;
+
+	InvMedTree<FMM_Mat_t> *temp= new InvMedTree<FMM_Mat_t>(comm);	
+	temp->bndry = bndry;
+	temp->kernel = kernel;
+	temp->fn = zero_fn;
+	temp->f_max = 1;
 
 	InvMedTree<FMM_Mat_t> *eta = new InvMedTree<FMM_Mat_t>(comm);	
 	eta->bndry = bndry;
@@ -111,13 +244,17 @@ int main(int argc, char* argv[]){
 	phi_0->Copy_FMMOutput();
   phi_0->Write2File("results/phi_0",0);
 
+	temp->SetupFMM(fmm_mat);
+
   // Copy phi_0 to phi and and set it up for FMM	
 	InvMedTree<FMM_Mat_t> *phi = new InvMedTree<FMM_Mat_t>(comm);	
 	phi->Copy(phi_0);
   phi->Write2File("results/phi_0_copy",0);
 	phi->SetupFMM(fmm_mat);
 	
-	// compute phi using born approx
+  // -------------------------------------------------------------------
+  // Compute phi using the Born approximation
+  // -------------------------------------------------------------------
 	phi->Multiply(eta,-1);  
 	phi->RunFMM();
 	phi->Copy_FMMOutput();
@@ -126,6 +263,76 @@ int main(int argc, char* argv[]){
 
 	phi->Add(phi_0,-1);
   phi->Write2File("results/born_difference",0);
+
+
+  // -------------------------------------------------------------------
+  // Set up the linear system
+  // -------------------------------------------------------------------
+	// Set up the operator for CG;
+	PetscInt m = phi_0->m;
+	PetscInt M = phi_0->M;
+	PetscInt n = phi_0->n;
+	PetscInt N = phi_0->N;
+	Mat A;
+	InvMedData invmed_data;
+	invmed_data.temp = temp;
+	invmed_data.phi_0 = phi_0;
+  MatCreateShell(comm,m,n,M,N,&invmed_data,&A);
+  MatShellSetOperation(A,MATOP_MULT,(void(*)(void))mult);
+	Vec sol ,rhs;
+  VecCreateMPI(comm,n,PETSC_DETERMINE,&sol);
+  VecCreateMPI(comm,n,PETSC_DETERMINE,&rhs);
+	tree2vec(phi,rhs);
+	//VecView(rhs, PETSC_VIEWER_STDOUT_SELF);
+
+  KSP ksp;
+	ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);
+  ierr = KSPSetOperators(ksp,A,A,DIFFERENT_NONZERO_PATTERN);CHKERRQ(ierr);
+  KSPSetType(ksp  ,KSPGMRES);
+  KSPSetNormType(ksp  , KSP_NORM_UNPRECONDITIONED);
+  KSPSetTolerances(ksp  ,GMRES_TOL  ,PETSC_DEFAULT,PETSC_DEFAULT,MAX_ITER  );
+  //KSPGMRESSetRestart(ksp  , MAX_ITER  );
+  KSPGMRESSetRestart(ksp  , 100  );
+  ierr = KSPSetFromOptions(ksp  );CHKERRQ(ierr);
+	ierr = KSPMonitorSet(ksp, KSPMonitorDefault, NULL, NULL); CHKERRQ(ierr);
+
+
+	double time_ksp;
+	int    iter_ksp;
+  // -------------------------------------------------------------------
+  // Solve the linear system
+  // -------------------------------------------------------------------
+  pvfmm::Profile::Tic("KSPSolve",&comm,true);
+  time_ksp=-omp_get_wtime();
+  ierr = KSPSolve(ksp,rhs,sol);CHKERRQ(ierr);
+  MPI_Barrier(comm);
+  time_ksp+=omp_get_wtime();
+  pvfmm::Profile::Toc();
+
+  // View info about the solver
+  KSPView(ksp,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+
+  // -------------------------------------------------------------------
+  // Check solution and clean up
+  // -------------------------------------------------------------------
+
+  // Iterations
+  PetscInt       its;
+  ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Iterations %D\n",its);CHKERRQ(ierr);
+  iter_ksp=its;
+
+  { // Write output
+    vec2tree(sol, phi);
+    phi->Write2File("results/sol",0);
+  }
+
+  // Free work space.  All PETSc objects should be destroyed when they
+  // are no longer needed.
+  ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
+  ierr = VecDestroy(&sol);CHKERRQ(ierr);
+  ierr = VecDestroy(&rhs);CHKERRQ(ierr);
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
 
 	return 0;
 }

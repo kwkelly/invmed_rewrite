5d7efe93b553284fa3f3e5469c758fcd13f8343b
Keith Kelly <keith@ices.utexas.edu>
2014-10-20 11:29:26 -0500

    Switched out #pragma once for old style header guard

--- include/typedefs.hpp
+++ include/typedefs.hpp
@@ -1,5 +1,9 @@
-#include <pvfmm_common.hpp>
+#ifndef TYPEDEFS_HPP
+#define TYPEDEFS_HPP
 
-#pragma once
+//#include <pvfmm_common.hpp>
+
 typedef pvfmm::FMM_Node<pvfmm::Cheb_Node<double> > FMMNode_t;
 typedef pvfmm::FMM_Cheb<FMMNode_t> FMM_Mat_t;
+
+#endif

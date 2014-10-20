1643cf3c9d666e8586751d755a20603f446e3513
Keith Kelly <keith@ices.utexas.edu>
2014-10-20 11:35:14 -0500

    Removes some of the utility functions from the top of the invmed.cpp file and moves them to invmed_utils.hpp. Declaration and definition included in header (for simplicity). Adds option for full or only partial observations (a bit of a hack, needs to be improved).

---
+++ include/invmed_utils.hpp
@@ -0,0 +1,364 @@
+/////////////////////////////////////////
+// Declarations
+/////////////////////////////////////////
+struct InvMedData{
+	InvMedTree<FMM_Mat_t>* phi_0;
+	InvMedTree<FMM_Mat_t>* temp;
+	pvfmm::PtFMM_Tree* pt_tree;
+	std::vector<double> src_coord;
+};
+
+#undef __FUNCT__
+#define __FUNCT__ "mult"
+int mult(Mat M, Vec U, Vec Y);
+
+#undef __FUNCT__
+#define __FUNCT__ "fullmult"
+int fullmult(Mat M, Vec U, Vec Y);
+
+#undef __FUNCT__
+#define __FUNCT__ "tree2vec"
+template <class FMM_Mat_t>
+int tree2vec(InvMedTree<FMM_Mat_t> *tree, Vec& Y);
+
+#undef __FUNCT__
+#define __FUNCT__ "vec2tree"
+template <class FMM_Mat_t>
+int vec2tree(Vec& Y, InvMedTree<FMM_Mat_t> *tree);
+
+void helm_kernel_fn_var(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr, double k);
+void helm_kernel_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr);
+void helm_kernel_conj_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr);
+void nonsingular_kernel_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr);
+
+std::vector<double> randsph(int n_points, double rad);
+std::vector<double> randunif(int n_points);
+
+
+const pvfmm::Kernel<double> helm_kernel=pvfmm::BuildKernel<double, helm_kernel_fn>("helm_kernel", 3, std::pair<int,int>(2,2));
+const pvfmm::Kernel<double> helm_kernel_conj=pvfmm::BuildKernel<double, helm_kernel_conj_fn>("helm_kernel_conj", 3, std::pair<int,int>(2,2));
+const pvfmm::Kernel<double> nonsingular_kernel=pvfmm::BuildKernel<double, nonsingular_kernel_fn>("nonsingular_kernel", 3, std::pair<int,int>(2,2));
+
+std::vector<double> test_pts();
+
+/////////////////////////////////////////
+// Definitions
+/////////////////////////////////////////
+
+void helm_kernel_fn_var(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr, double k){
+#ifndef __MIC__
+	pvfmm::Profile::Add_FLOP((long long)trg_cnt*(long long)src_cnt*(24*dof));
+#endif
+	for(int t=0;t<trg_cnt;t++){
+		for(int i=0;i<dof;i++){
+			double p[2]={0,0};
+			for(int s=0;s<src_cnt;s++){
+				double dX_reg=r_trg[3*t ]-r_src[3*s ];
+				double dY_reg=r_trg[3*t+1]-r_src[3*s+1];
+				double dZ_reg=r_trg[3*t+2]-r_src[3*s+2];
+				double R = (dX_reg*dX_reg+dY_reg*dY_reg+dZ_reg*dZ_reg);
+				if (R!=0){
+					R = sqrt(R);
+					double invR=1.0/R;
+					invR = invR/(4*const_pi<double>());
+					double G[2]={cos(k*R)*invR, sin(k*R)*invR};
+					p[0] += v_src[(s*dof+i)*2+0]*G[0] - v_src[(s*dof+i)*2+1]*G[1];
+					p[1] += v_src[(s*dof+i)*2+0]*G[1] + v_src[(s*dof+i)*2+1]*G[0];
+				}
+			}
+			k_out[(t*dof+i)*2+0] += p[0];
+			k_out[(t*dof+i)*2+1] += p[1];
+		}
+	}
+}
+
+void helm_kernel_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr){
+	helm_kernel_fn_var(r_src, src_cnt, v_src, dof, r_trg, trg_cnt, k_out, mem_mgr, 1);
+};
+
+void helm_kernel_conj_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr){
+	helm_kernel_fn_var(r_src, src_cnt, v_src, dof, r_trg, trg_cnt, k_out, mem_mgr, -1);
+};
+
+
+void nonsingular_kernel_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr){
+#ifndef __MIC__
+	pvfmm::Profile::Add_FLOP((long long)trg_cnt*(long long)src_cnt*(24*dof));
+#endif
+	for(int t=0;t<trg_cnt;t++){
+		for(int i=0;i<dof;i++){
+			double p[2]={0,0};
+			for(int s=0;s<src_cnt;s++){
+				double dX_reg=r_trg[3*t ]-r_src[3*s ];
+				double dY_reg=r_trg[3*t+1]-r_src[3*s+1];
+				double dZ_reg=r_trg[3*t+2]-r_src[3*s+2];
+				double R = (dX_reg*dX_reg+dY_reg*dY_reg+dZ_reg*dZ_reg);
+				if (R!=0){
+					R = sqrt(R);
+					double invR=1.0/R;
+					invR = invR/(4*const_pi<double>());
+					double G[2]={cos(R), sin(R)};
+					p[0] += v_src[(s*dof+i)*2+0]*G[0] - v_src[(s*dof+i)*2+1]*G[1];
+					p[1] += v_src[(s*dof+i)*2+0]*G[1] + v_src[(s*dof+i)*2+1]*G[0];
+				}
+			}
+			k_out[(t*dof+i)*2+0] += p[0];
+			k_out[(t*dof+i)*2+1] += p[1];
+		}
+	}
+}
+
+std::vector<double> randsph(int n_points, double rad){
+	std::vector<double> src_points;
+	std::vector<double> z;
+	std::vector<double> phi;
+	std::vector<double> theta;
+	double val;
+
+	std::srand(std::time(NULL));
+	for (int i = 0; i < n_points; i++) {
+		val = 2*rad*((double)std::rand()/(double)RAND_MAX) - rad;
+		z.push_back(val);
+		theta.push_back(asin(z[i]/rad));
+		val = 2*M_PI*((double)std::rand()/(double)RAND_MAX);
+		phi.push_back(val);
+		src_points.push_back(rad*cos(theta[i])*cos(phi[i])+.5);
+		src_points.push_back(rad*cos(theta[i])*sin(phi[i])+.5);
+		src_points.push_back(z[i]+.5);
+	}
+
+	return src_points;
+}
+
+std::vector<double> randunif(int n_points){
+	double val;
+	std::vector<double> src_points;
+
+	std::srand(std::time(NULL));
+	for (int i = 0; i < n_points; i++) {
+		val = ((double)std::rand()/(double)RAND_MAX);
+		src_points.push_back(val);
+		val = ((double)std::rand()/(double)RAND_MAX);
+		src_points.push_back(val);
+		val = ((double)std::rand()/(double)RAND_MAX);
+		src_points.push_back(val);
+	}
+
+	return src_points;
+}
+
+std::vector<double> test_pts(){
+	std::vector<double> pts;
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.3000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.3000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.3000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.3000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.3000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.3000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.7000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.7000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.7000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.7000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.7000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.7000);
+	pts.push_back( 0.5000);
+	pts.push_back( 0.5000);
+	return pts;
+}
+
+
+#undef __FUNCT__
+#define __FUNCT__ "fullmult"
+int fullmult(Mat M, Vec U, Vec Y){
+
+	PetscErrorCode ierr;
+	// Get context
+	InvMedData *invmed_data = NULL;
+	MatShellGetContext(M, &invmed_data);
+	InvMedTree<FMM_Mat_t>* phi_0 = invmed_data->phi_0;
+	InvMedTree<FMM_Mat_t>* temp = invmed_data->temp;
+
+	const MPI_Comm* comm=invmed_data->phi_0->Comm();
+	int cheb_deg = InvMedTree<FMM_Mat_t>::cheb_deg;
+	//int omp_p=omp_get_max_threads();
+	vec2tree(U,temp);
+
+	temp->Multiply(phi_0,1);
+
+	// Run FMM ( Compute: G[ \eta * u ] )
+	temp->ClearFMMData();
+	temp->RunFMM();
+	temp->Copy_FMMOutput();
+
+	// Regularize
+	tree2vec(temp,Y);
+
+	PetscScalar alpha = (PetscScalar)1e-10;
+	ierr = VecAXPY(Y,alpha,U);CHKERRQ(ierr);
+
+	return 0;
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "mult"
+int mult(Mat M, Vec U, Vec Y){
+
+	PetscErrorCode ierr;
+	// Get context
+	InvMedData *invmed_data = NULL;
+	MatShellGetContext(M, &invmed_data);
+	InvMedTree<FMM_Mat_t>* phi_0 = invmed_data->phi_0;
+	InvMedTree<FMM_Mat_t>* temp = invmed_data->temp;
+	pvfmm::PtFMM_Tree* pt_tree = invmed_data->pt_tree;
+	std::vector<double> src_coord = invmed_data->src_coord;
+
+	const MPI_Comm* comm=invmed_data->phi_0->Comm();
+	int cheb_deg = InvMedTree<FMM_Mat_t>::cheb_deg;
+	//int omp_p=omp_get_max_threads();
+	
+	vec2tree(U,temp);
+
+//	temp->Multiply(phi_0,1);
+
+	// Run FMM ( Compute: G[ \eta * u ] )
+	temp->ClearFMMData();
+	temp->RunFMM();
+	temp->Copy_FMMOutput();
+
+	// Sample at the points in src_coord, then apply the transpose
+	// operator.
+	std::vector<double> src_values = temp->ReadVals(src_coord);
+	std::cout << "vals: " << src_values[0] << ", " << src_values[1] << std::endl;
+	std::cout << "force them to be correct" << std::endl;
+
+	src_values[0] = -1;
+	src_values[1] = 0;
+
+//	std::cout << "src_values[i]" << std::endl;
+//	for(int i=0;i<src_values.size();i++){
+//		std::cout << src_values[i] << std::endl;
+//	}
+	//InvMedTree<FMM_Mat_t>::SetSrcValues(src_coord,src_values,pt_tree);
+
+	std::vector<double> trg_value;
+	pvfmm::PtFMM_Evaluate(pt_tree, trg_value, 0, &src_values);
+
+	// Insert the values back in
+	temp->Trg2Tree(trg_value);
+	
+	// Ptwise multiply by the conjugate of phi_0
+//	temp->ConjMultiply(phi_0,1);
+
+	temp->Write2File("results/aftermult",0);
+
+
+	// Regularize
+	tree2vec(temp,Y);
+	
+
+	PetscScalar alpha = (PetscScalar)0;
+	ierr = VecAXPY(Y,alpha,U);CHKERRQ(ierr);
+
+	return 0;
+}
+		
+#undef __FUNCT__
+#define __FUNCT__ "tree2vec"
+template <class FMM_Mat_t>
+int tree2vec(InvMedTree<FMM_Mat_t> *tree, Vec& Y){
+	PetscErrorCode ierr;
+	int cheb_deg=InvMedTree<FMM_Mat_t>::cheb_deg;
+
+	std::vector<FMMNode_t*> nlist = tree->GetNGLNodes();
+
+	int omp_p=omp_get_max_threads();
+	size_t n_coeff3=(cheb_deg+1)*(cheb_deg+2)*(cheb_deg+3)/6;
+
+	{
+		PetscInt Y_size;
+		ierr = VecGetLocalSize(Y, &Y_size);
+		int data_dof=Y_size/(n_coeff3*nlist.size());
+		int SCAL_EXP = 1;
+
+		PetscScalar *Y_ptr;
+		ierr = VecGetArray(Y, &Y_ptr);
+
+		#pragma omp parallel for
+		for(size_t tid=0;tid<omp_p;tid++){
+			size_t i_start=(nlist.size()* tid   )/omp_p;
+			size_t i_end  =(nlist.size()*(tid+1))/omp_p;
+			for(size_t i=i_start;i<i_end;i++){
+				pvfmm::Vector<double>& coeff_vec=nlist[i]->ChebData();
+				double s=std::pow(0.5,COORD_DIM*nlist[i]->Depth()*0.5*SCAL_EXP);
+
+				size_t Y_offset=i*n_coeff3*data_dof;
+				for(size_t j=0;j<n_coeff3*data_dof;j++) Y_ptr[j+Y_offset]=coeff_vec[j]*s;
+			}
+		}
+		ierr = VecRestoreArray(Y, &Y_ptr);
+	}
+
+	return 0;
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "vec2tree"
+template <class FMM_Mat_t>
+int vec2tree(Vec& Y, InvMedTree<FMM_Mat_t> *tree){
+	PetscErrorCode ierr;
+	const MPI_Comm* comm=tree->Comm();
+	int cheb_deg = InvMedTree<FMM_Mat_t>::cheb_deg;
+
+	std::vector<FMMNode_t*> nlist = tree->GetNGLNodes();
+
+	int omp_p=omp_get_max_threads();
+	size_t n_coeff3=(cheb_deg+1)*(cheb_deg+2)*(cheb_deg+3)/6;
+
+	{
+		PetscInt Y_size;
+		ierr = VecGetLocalSize(Y, &Y_size);
+		int data_dof=Y_size/(n_coeff3*nlist.size());
+		int SCAL_EXP = 1;
+
+		const PetscScalar *Y_ptr;
+		ierr = VecGetArrayRead(Y, &Y_ptr);
+
+		#pragma omp parallel for
+		for(size_t tid=0;tid<omp_p;tid++){
+			size_t i_start=(nlist.size()* tid   )/omp_p;
+			size_t i_end  =(nlist.size()*(tid+1))/omp_p;
+			for(size_t i=i_start;i<i_end;i++){
+				pvfmm::Vector<double>& coeff_vec=nlist[i]->ChebData();
+				double s=std::pow(2.0,COORD_DIM*nlist[i]->Depth()*0.5*SCAL_EXP);
+
+				size_t Y_offset=i*n_coeff3*data_dof;
+				for(size_t j=0;j<n_coeff3*data_dof;j++) coeff_vec[j]=PetscRealPart(Y_ptr[j+Y_offset])*s;
+				nlist[i]->DataDOF()=data_dof;
+			}
+		}
+	}
+
+	return 0;
+}
--- src/invmed.cpp
+++ src/invmed.cpp
@@ -8,245 +8,10 @@
 //#include "petsc_utils.hpp"
 #include "typedefs.hpp"
 #include <pvfmm.hpp>
-#include <pvfmm_common.hpp>
+//#include <pvfmm_common.hpp>
+#include "invmed_utils.hpp"
+
 //BEGIN COPY FROM UTILS
-struct InvMedData{
-	InvMedTree<FMM_Mat_t>* phi_0;
-	InvMedTree<FMM_Mat_t>* temp;
-	pvfmm::PtFMM_Tree* pt_tree;
-	std::vector<double> src_coord;
-};
-
-#undef __FUNCT__
-#define __FUNCT__ "mult"
-int mult(Mat M, Vec U, Vec Y);
-
-#undef __FUNCT__
-#define __FUNCT__ "tree2vec"
-template <class FMM_Mat_t>
-int tree2vec(InvMedTree<FMM_Mat_t> *tree, Vec& Y);
-
-#undef __FUNCT__
-#define __FUNCT__ "vec2tree"
-template <class FMM_Mat_t>
-int vec2tree(Vec& Y, InvMedTree<FMM_Mat_t> *tree);
-
-void helm_kernel_fn_var(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr, double k);
-void helm_kernel_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr);
-void helm_kernel_conj_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr);
-
-void helm_kernel_fn_var(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr, double k){
-#ifndef __MIC__
-	pvfmm::Profile::Add_FLOP((long long)trg_cnt*(long long)src_cnt*(24*dof));
-#endif
-	for(int t=0;t<trg_cnt;t++){
-		for(int i=0;i<dof;i++){
-			double p[2]={0,0};
-			for(int s=0;s<src_cnt;s++){
-				double dX_reg=r_trg[3*t ]-r_src[3*s ];
-				double dY_reg=r_trg[3*t+1]-r_src[3*s+1];
-				double dZ_reg=r_trg[3*t+2]-r_src[3*s+2];
-				double R = (dX_reg*dX_reg+dY_reg*dY_reg+dZ_reg*dZ_reg);
-				if (R!=0){
-					R = sqrt(R);
-					double invR=1.0/R;
-					invR = invR/(4*const_pi<double>());
-					double G[2]={cos(k*R)*invR, sin(k*R)*invR};
-					p[0] += v_src[(s*dof+i)*2+0]*G[0] - v_src[(s*dof+i)*2+1]*G[1];
-					p[1] += v_src[(s*dof+i)*2+0]*G[1] + v_src[(s*dof+i)*2+1]*G[0];
-				}
-			}
-			k_out[(t*dof+i)*2+0] += p[0];
-			k_out[(t*dof+i)*2+1] += p[1];
-		}
-	}
-}
-
-void helm_kernel_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr){
-	helm_kernel_fn_var(r_src, src_cnt, v_src, dof, r_trg, trg_cnt, k_out, mem_mgr, 1);
-};
-
-void helm_kernel_conj_fn(double* r_src, int src_cnt, double* v_src, int dof, double* r_trg, int trg_cnt, double* k_out, pvfmm::mem::MemoryManager* mem_mgr){
-	helm_kernel_fn_var(r_src, src_cnt, v_src, dof, r_trg, trg_cnt, k_out, mem_mgr, -1);
-};
-
-const pvfmm::Kernel<double> helm_kernel=pvfmm::BuildKernel<double, helm_kernel_fn>("helm_kernel", 3, std::pair<int,int>(2,2));
-const pvfmm::Kernel<double> helm_kernel_conj=pvfmm::BuildKernel<double, helm_kernel_conj_fn>("helm_kernel_conj", 3, std::pair<int,int>(2,2));
-
-std::vector<double> randsph(int n_points, double rad);
-std::vector<double> randunif(int n_points);
-
-std::vector<double> randsph(int n_points, double rad){
-	std::vector<double> src_points;
-	std::vector<double> z;
-	std::vector<double> phi;
-	std::vector<double> theta;
-	double val;
-
-	std::srand(std::time(NULL));
-	for (int i = 0; i < n_points; i++) {
-		val = 2*rad*((double)std::rand()/(double)RAND_MAX) - rad;
-		z.push_back(val);
-		theta.push_back(asin(z[i]/rad));
-		val = 2*M_PI*((double)std::rand()/(double)RAND_MAX);
-		phi.push_back(val);
-		src_points.push_back(rad*cos(theta[i])*cos(phi[i])+.5);
-		src_points.push_back(rad*cos(theta[i])*sin(phi[i])+.5);
-		src_points.push_back(z[i]+.5);
-	}
-
-	return src_points;
-}
-
-std::vector<double> randunif(int n_points){
-	double val;
-	std::vector<double> src_points;
-
-	std::srand(std::time(NULL));
-	for (int i = 0; i < n_points; i++) {
-		val = ((double)std::rand()/(double)RAND_MAX);
-		src_points.push_back(val);
-		val = ((double)std::rand()/(double)RAND_MAX);
-		src_points.push_back(val);
-		val = ((double)std::rand()/(double)RAND_MAX);
-		src_points.push_back(val);
-	}
-
-	return src_points;
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "mult"
-int mult(Mat M, Vec U, Vec Y){
-
-	PetscErrorCode ierr;
-	// Get context
-	InvMedData *invmed_data = NULL;
-	MatShellGetContext(M, &invmed_data);
-	InvMedTree<FMM_Mat_t>* phi_0 = invmed_data->phi_0;
-	InvMedTree<FMM_Mat_t>* temp = invmed_data->temp;
-	pvfmm::PtFMM_Tree* pt_tree = invmed_data->pt_tree;
-	std::vector<double> src_coord = invmed_data->src_coord;
-
-	const MPI_Comm* comm=invmed_data->phi_0->Comm();
-	int cheb_deg = InvMedTree<FMM_Mat_t>::cheb_deg;
-	//int omp_p=omp_get_max_threads();
-	vec2tree(U,temp);
-
-	temp->Multiply(phi_0,1);
-
-	// Run FMM ( Compute: G[ \eta * u ] )
-	temp->ClearFMMData();
-	temp->RunFMM();
-	temp->Copy_FMMOutput();
-
-	// Sample at the points in src_coord, then apply the transpose
-	// operator.
-	std::vector<double> src_values = temp->ReadVals(src_coord);
-
-//	std::cout << "src_values[i]" << std::endl;
-//	for(int i=0;i<src_values.size();i++){
-//		std::cout << src_values[i] << std::endl;
-//	}
-	//InvMedTree<FMM_Mat_t>::SetSrcValues(src_coord,src_values,pt_tree);
-
-	std::vector<double> trg_value;
-	pvfmm::PtFMM_Evaluate(pt_tree, trg_value, 0, &src_values);
-
-	// Insert the values back in
-	temp->Trg2Tree(trg_value);
-	
-	// Ptwise multiply by the conjugate of phi_0
-	temp->ConjMultiply(phi_0,1);
-
-
-	// Regularize
-	tree2vec(temp,Y);
-
-	PetscScalar alpha = (PetscScalar)1e-10;
-	ierr = VecAXPY(Y,alpha,U);CHKERRQ(ierr);
-
-	return 0;
-}
-		
-#undef __FUNCT__
-#define __FUNCT__ "tree2vec"
-template <class FMM_Mat_t>
-int tree2vec(InvMedTree<FMM_Mat_t> *tree, Vec& Y){
-	PetscErrorCode ierr;
-	int cheb_deg=InvMedTree<FMM_Mat_t>::cheb_deg;
-
-	std::vector<FMMNode_t*> nlist = tree->GetNGLNodes();
-
-	int omp_p=omp_get_max_threads();
-	size_t n_coeff3=(cheb_deg+1)*(cheb_deg+2)*(cheb_deg+3)/6;
-
-	{
-		PetscInt Y_size;
-		ierr = VecGetLocalSize(Y, &Y_size);
-		int data_dof=Y_size/(n_coeff3*nlist.size());
-		int SCAL_EXP = 1;
-
-		PetscScalar *Y_ptr;
-		ierr = VecGetArray(Y, &Y_ptr);
-
-		#pragma omp parallel for
-		for(size_t tid=0;tid<omp_p;tid++){
-			size_t i_start=(nlist.size()* tid   )/omp_p;
-			size_t i_end  =(nlist.size()*(tid+1))/omp_p;
-			for(size_t i=i_start;i<i_end;i++){
-				pvfmm::Vector<double>& coeff_vec=nlist[i]->ChebData();
-				double s=std::pow(0.5,COORD_DIM*nlist[i]->Depth()*0.5*SCAL_EXP);
-
-				size_t Y_offset=i*n_coeff3*data_dof;
-				for(size_t j=0;j<n_coeff3*data_dof;j++) Y_ptr[j+Y_offset]=coeff_vec[j]*s;
-			}
-		}
-		ierr = VecRestoreArray(Y, &Y_ptr);
-	}
-
-	return 0;
-}
-
-#undef __FUNCT__
-#define __FUNCT__ "vec2tree"
-template <class FMM_Mat_t>
-int vec2tree(Vec& Y, InvMedTree<FMM_Mat_t> *tree){
-	PetscErrorCode ierr;
-	const MPI_Comm* comm=tree->Comm();
-	int cheb_deg = InvMedTree<FMM_Mat_t>::cheb_deg;
-
-	std::vector<FMMNode_t*> nlist = tree->GetNGLNodes();
-
-	int omp_p=omp_get_max_threads();
-	size_t n_coeff3=(cheb_deg+1)*(cheb_deg+2)*(cheb_deg+3)/6;
-
-	{
-		PetscInt Y_size;
-		ierr = VecGetLocalSize(Y, &Y_size);
-		int data_dof=Y_size/(n_coeff3*nlist.size());
-		int SCAL_EXP = 1;
-
-		const PetscScalar *Y_ptr;
-		ierr = VecGetArrayRead(Y, &Y_ptr);
-
-		#pragma omp parallel for
-		for(size_t tid=0;tid<omp_p;tid++){
-			size_t i_start=(nlist.size()* tid   )/omp_p;
-			size_t i_end  =(nlist.size()*(tid+1))/omp_p;
-			for(size_t i=i_start;i<i_end;i++){
-				pvfmm::Vector<double>& coeff_vec=nlist[i]->ChebData();
-				double s=std::pow(2.0,COORD_DIM*nlist[i]->Depth()*0.5*SCAL_EXP);
-
-				size_t Y_offset=i*n_coeff3*data_dof;
-				for(size_t j=0;j<n_coeff3*data_dof;j++) coeff_vec[j]=PetscRealPart(Y_ptr[j+Y_offset])*s;
-				nlist[i]->DataDOF()=data_dof;
-			}
-		}
-	}
-
-	return 0;
-}
 
 //END COPY FROM UTILS
 #undef __FUNCT__
@@ -260,8 +25,9 @@ int main(int argc, char* argv[]){
 												-max_depth  <Int>    Maximum tree depth\n\
 												-fmm_q      <Int>    Chebyshev polynomial degree\n\
 												-fmm_m      <Int>    Multipole order (+ve even integer)\n\
-												-gmres_tol  <Real>   GMRES residual tolerance\n\
-												-gmres_iter <Int>    GMRES maximum iterations\n\
+												-tol 			  <Real>   GMRES/CG residual tolerance\n\
+												-iter 			<Int>    GMRES/CG maximum iterations\n\
+												-obs				<Int>		 0 for partial, 1 for full\n\
 												";
 	PetscInt  VTK_ORDER=0;
 	PetscInt  INPUT_DOF=2;
@@ -270,14 +36,15 @@ int main(int argc, char* argv[]){
 	PetscBool TREE_ONLY=PETSC_FALSE;
 	PetscInt  MAXDEPTH  =MAX_DEPTH;// Maximum tree depth
 	PetscInt  MINDEPTH   =4;       // Minimum tree depth
-	PetscReal       TOL  =1e-3;    // Tolerance
+	PetscReal   REF_TOL  =1e-3;    // Tolerance
 	//PetscReal GMRES_TOL  =1e-6;   // Fine mesh GMRES tolerance
-	PetscReal CG_TOL  =1e-6;    	// Fine mesh CG tolerance
+	PetscReal 		TOL  =1e-6;    	// Fine mesh GMRES/CG tolerance
 	PetscInt  CHEB_DEG  =14;       // Fine mesh Cheb. order
 	PetscInt MUL_ORDER  =10;       // Fine mesh mult  order
 	PetscInt MAX_ITER  =200;
 	PetscReal f_max=1;
 	PetscReal eta_=1;
+	PetscInt OBS = 1;
 
   PetscErrorCode ierr;
   PetscInitialize(&argc,&argv,0,help);
@@ -293,19 +60,15 @@ int main(int argc, char* argv[]){
   PetscOptionsGetReal(NULL,       "-scal",& SCAL_EXP  ,NULL);
   PetscOptionsGetBool(NULL,   "-periodic",& PERIODIC  ,NULL);
   PetscOptionsGetBool(NULL,       "-tree",& TREE_ONLY ,NULL);
-
   PetscOptionsGetInt (NULL, "-max_depth" ,&MAXDEPTH   ,NULL);
   PetscOptionsGetInt (NULL, "-min_depth" ,&MINDEPTH   ,NULL);
-  PetscOptionsGetReal(NULL,   "-ref_tol" ,&      TOL  ,NULL);
-  //PetscOptionsGetReal(NULL, "-gmres_tol" ,&GMRES_TOL  ,NULL);
-  PetscOptionsGetReal(NULL, "-cg_tol" ,&CG_TOL  ,NULL);
-
+  PetscOptionsGetReal(NULL,   "-ref_tol" ,&  REF_TOL  ,NULL);
+  PetscOptionsGetReal(NULL, "-tol" ,		  			&TOL  ,NULL);
   PetscOptionsGetInt (NULL,   "-fmm_q"   ,& CHEB_DEG  ,NULL);
   PetscOptionsGetInt (NULL,   "-fmm_m"   ,&MUL_ORDER  ,NULL);
-
-  PetscOptionsGetInt (NULL, "-gmres_iter",& MAX_ITER  ,NULL);
-
+  PetscOptionsGetInt (NULL, "-iter",&       MAX_ITER  ,NULL);
   PetscOptionsGetReal(NULL,       "-eta" ,&    eta_   ,NULL);
+  PetscOptionsGetInt (NULL, "-obs",&             OBS  ,NULL);
 
 	pvfmm::Profile::Enable(true);
 
@@ -321,7 +84,7 @@ int main(int argc, char* argv[]){
 	// Define static variables
 	InvMedTree<FMM_Mat_t>::cheb_deg = CHEB_DEG;
 	InvMedTree<FMM_Mat_t>::mult_order = MUL_ORDER;
-	InvMedTree<FMM_Mat_t>::tol = TOL;
+	InvMedTree<FMM_Mat_t>::tol = REF_TOL;
 	InvMedTree<FMM_Mat_t>::mindepth = MINDEPTH;
 	InvMedTree<FMM_Mat_t>::maxdepth = MAXDEPTH;
 	InvMedTree<FMM_Mat_t>::adap = true;
@@ -362,10 +125,12 @@ int main(int argc, char* argv[]){
 	// total num of nodes (non ghost, leaf);
 	int n_tnodes = (eta->GetNGLNodes()).size();
 	int total_size = n_nodes3*n_tnodes;
+	std::cout << "total_size: " << total_size << std::endl;
 
-	std::vector<double> src_coord;
-	src_coord = randsph(total_size,.12);
+	//std::vector<double> src_coord;
+	//src_coord = randsph(total_size,.12);
 	//src_coord = randunif(total_size);
+	std::vector<double> src_coord = test_pts();
 	std::cout << "size: " << src_coord.size() << std::endl;
 
 
@@ -373,7 +138,7 @@ int main(int argc, char* argv[]){
 	fmm_mat->Initialize(InvMedTree<FMM_Mat_t>::mult_order,InvMedTree<FMM_Mat_t>::cheb_deg,comm,kernel);
 	eta->Write2File("results/eta",0);
 	phi_0->Write2File("results/pt_sources",0);
-	// compute phi_0
+	// compute phi_0 from the pt_sources
 	phi_0->SetupFMM(fmm_mat);
 	phi_0->RunFMM();
 	phi_0->Copy_FMMOutput();
@@ -403,19 +168,20 @@ int main(int argc, char* argv[]){
   phi->Write2File("results/born_difference",0);
 
 	// Sample phi at the points in src_coord, then apply the transpose
-	// operator.
-	
-	std::vector<double> phi_samples = phi->ReadVals(src_coord);
-	pvfmm::PtFMM_Tree* pt_tree = phi->CreatePtFMMTree(src_coord, phi_samples, kernel_conj);
+	// operator if we have only partial observations.
+		std::vector<double> phi_samples = phi->ReadVals(src_coord);
+		pvfmm::PtFMM_Tree* pt_tree = phi->CreatePtFMMTree(src_coord, phi_samples, kernel_conj);
 
-	std::vector<double> trg_value;
-	pvfmm::PtFMM_Evaluate(pt_tree, trg_value);
+	if(OBS == 0){	
+		std::vector<double> trg_value;
+		pvfmm::PtFMM_Evaluate(pt_tree, trg_value);
 
-	// Insert the values back in
-	phi->Trg2Tree(trg_value);
-	phi->ConjMultiply(phi_0,1);
-	//phi->ScalarMultiply(10000000);
-	phi->Write2File("results/rhs_after_adj",0);
+		// Insert the values back in
+		phi->Trg2Tree(trg_value);
+		phi->ConjMultiply(phi_0,1);
+		//phi->ScalarMultiply(10000000);
+		phi->Write2File("results/rhs_after_adj",0);
+	}
 
   // -------------------------------------------------------------------
   // Set up the linear system
@@ -429,10 +195,17 @@ int main(int argc, char* argv[]){
 	InvMedData invmed_data;
 	invmed_data.temp = temp;
 	invmed_data.phi_0 = phi_0;
-	invmed_data.pt_tree = pt_tree;
-	invmed_data.src_coord = src_coord;
-  MatCreateShell(comm,m,n,M,N,&invmed_data,&A);
-  MatShellSetOperation(A,MATOP_MULT,(void(*)(void))mult);
+	if(OBS == 0){
+		invmed_data.pt_tree = pt_tree;
+		invmed_data.src_coord = src_coord;
+		MatCreateShell(comm,m,n,M,N,&invmed_data,&A);
+		MatShellSetOperation(A,MATOP_MULT,(void(*)(void))mult);
+		MatSetOption(A,MAT_SPD,PETSC_TRUE);
+	}
+	else{
+		MatCreateShell(comm,m,n,M,N,&invmed_data,&A);
+		MatShellSetOperation(A,MATOP_MULT,(void(*)(void))fullmult);
+	}
 	Vec sol ,rhs;
   VecCreateMPI(comm,n,PETSC_DETERMINE,&sol);
   VecCreateMPI(comm,n,PETSC_DETERMINE,&rhs);
@@ -442,8 +215,13 @@ int main(int argc, char* argv[]){
   KSP ksp;
 	ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);
   ierr = KSPSetOperators(ksp,A,A,DIFFERENT_NONZERO_PATTERN);CHKERRQ(ierr);
-  //KSPSetType(ksp  ,KSPGMRES);
-  KSPSetType(ksp  ,KSPCG);
+
+	if(OBS==0){
+		KSPSetType(ksp  ,KSPCG);
+	}
+	else{
+		KSPSetType(ksp  ,KSPGMRES);
+	}
   KSPSetNormType(ksp  , KSP_NORM_UNPRECONDITIONED);
 	/*
 	 * PetscErrorCode  KSPSetTolerances(KSP ksp,PetscReal rtol,PetscReal abstol,PetscReal dtol,PetscInt maxits)
@@ -454,14 +232,20 @@ int main(int argc, char* argv[]){
 	 * dtol 	- the divergence tolerance, amount (possibly preconditioned) residual norm can increase before KSPConvergedDefault() concludes that the method is diverging
 	 * maxits 	- maximum number of iterations to use
 	 */
-  KSPSetTolerances(ksp  ,0  ,PETSC_DEFAULT,PETSC_DEFAULT,MAX_ITER  );
-  //KSPGMRESSetRestart(ksp  , MAX_ITER  );
-  //KSPGMRESSetRestart(ksp  , 100  );
-	//KSPGMRESSetOrthogonalization(ksp,KSPGMRESModifiedGramSchmidtOrthogonalization);
+  KSPSetTolerances(ksp  ,TOL  ,PETSC_DEFAULT,PETSC_DEFAULT,MAX_ITER  );
+	// What type of CG should this be... I think hermitian??
+	
+	// SET CG OR GMRES OPTIONS
+	if(OBS == 0){
+		ierr = KSPCGSetType(ksp,KSP_CG_SYMMETRIC); CHKERRQ(ierr);
+	}
+	else{
+  	KSPGMRESSetRestart(ksp  , MAX_ITER  );
+  	KSPGMRESSetRestart(ksp  , MAX_ITER  );
+		KSPGMRESSetOrthogonalization(ksp,KSPGMRESModifiedGramSchmidtOrthogonalization);
+	}
   ierr = KSPSetFromOptions(ksp  );CHKERRQ(ierr);
 	ierr = KSPMonitorSet(ksp, KSPMonitorDefault, NULL, NULL); CHKERRQ(ierr);
-	// What type of CG should this be... I think hermitian??
-	ierr = KSPCGSetType(ksp,KSP_CG_SYMMETRIC); CHKERRQ(ierr);
 
 	double time_ksp;
 	int    iter_ksp;
@@ -477,6 +261,7 @@ int main(int argc, char* argv[]){
 
  	KSPConvergedReason reason;
 	ierr=KSPGetConvergedReason(ksp,&reason);CHKERRQ(ierr);
+	std::cout << reason << std::endl;
 	ierr=PetscPrintf(PETSC_COMM_WORLD,"KSPConvergedReason: %D\n", reason);CHKERRQ(ierr);
 
   // View info about the solver
@@ -503,11 +288,14 @@ int main(int argc, char* argv[]){
   VecCreateMPI(comm,n,PETSC_DETERMINE,&eta_vec);
 	tree2vec(eta,eta_vec);
 
-	PetscReal norm;
-	ierr = VecAXPY(eta_vec,-1,sol);CHKERRQ(ierr);
-	ierr = VecNorm(eta_vec,NORM_2,&norm); CHKERRQ(ierr);
+	PetscReal diffnorm;
+	PetscReal etanorm;
+	ierr = VecAXPY(sol,-1,eta_vec);CHKERRQ(ierr);
+	ierr = VecNorm(sol,NORM_2,&diffnorm); CHKERRQ(ierr);
+	ierr = VecNorm(eta_vec,NORM_2,&etanorm); CHKERRQ(ierr);
 
-	std::cout << "||sol - eta||_2 = " << norm << std::endl;
+
+	std::cout << "||sol - eta||_2/||eta||_2 = " << diffnorm/etanorm << std::endl;
 
 /*
 	// Some checks!!!! This checks that O(sol) = rhs

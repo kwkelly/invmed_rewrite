61074711bb568b30ebdfa61a4aa8e35ff1085e4c
Keith Kelly <keith@ices.utexas.edu>
2015-07-30 16:06:38 -0500

    Adds extra loops to the rmf.sh script

--- scripts/rmf.sh
+++ scripts/rmf.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 MAX_QUEUE=20
-WAIT_TIME=1
+WAIT_TIME=60
+MAX_TIME=01:00:00
 
-for DEPTH in `seq 1 25`
+for DEPTH in 4
 do
-	for R_D in 25
+	for R_D in 10
 	do
-		for R_S in 25
+		for R_S in 10
 		do
-			for N_PTS in 5
+			for N_S in 25
 			do
-				while : ; do
-					[[ $(squeue -u $USER | tail -n +1 | wc -l) -lt $MAX_QUEUE ]] && break
-					echo "Pausing until the queue empties enough to add a new one."
-					sleep $WAIT_TIME
-				done
-				R_DN=$(($R_D<$N_PTS*$N_PTS?$R_D:$N_PTS*$N_PTS))
-				R_SN=$(($R_S<$N_PTS*$N_PTS?$R_S:$N_PTS*$N_PTS))
-				echo $R_DN
+				#for N_D in 4
+				#do
+				for K in 10
+				do
+					while : ; do
+						[[ $(squeue -u $USER | tail -n +1 | wc -l) -lt $MAX_QUEUE ]] && break
+						echo "Pausing until the queue empties enough to add a new one."
+						sleep $WAIT_TIME
+					done
+					MIN_SD=$(($R_D<$R_S?$R_S:$R_S))
+					JOBNAME=faims-$DEPTH-$N_S-$R_D-$R_S-$K
+					#JOBNAME=compute_kernel
+					if [ "$MIN_SD" -ge  "$K" ]; then
 cat <<-EOS | sbatch
-				#!/bin/bash
+					#!/bin/bash
 
-				#SBATCH -J conv-$DEPTH-$R_D-$N_PTS
-				#SBATCH -o conv-$DEPTH-$R_D-$N_PTS.6.out
-				#SBATCH -n 16
-				#SBATCH -N 16
-				#SBATCH -p gpu
-				#SBATCH -t 00:60:00
-				##SBATCH --mail-user=keith@ices.utexas.edu
-				##SBATCH --mail-type=begin
-				##SBATCH --mail-type=end
-				#SBATCH -A PADAS
-				cd ~/projects/invmed_rewrite/build/
-				ibrun ls
+					#SBATCH -J $JOBNAME
+					#SBATCH -o $JOBNAME.out
+					#SBATCH -n 4
+					#SBATCH -N 4
+					#SBATCH -p gpu
+					#SBATCH -t $MAX_TIME
+					##SBATCH --mail-user=keith@ices.utexas.edu
+					##SBATCH --mail-type=begin
+					##SBATCH --mail-type=end
+					#SBATCH -A PADAS
+					cd ~/projects/invmed_rewrite/build/
+					ibrun ./faims -fmm_m 8 -fmm_q 6 -min_depth $DEPTH -max_depth $DEPTH -N_s $N_S -N_d $N_S -R_d $R_D -R_s $R_S -R_b $R_S -k $K
 
-				exit 0
-				EOS
-			done
+					exit 0
+					EOS
+				fi
+				done
+				done
+			#done
 		done
 	done
 done

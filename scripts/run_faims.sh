699a53337fb07357d4d09bd4da157453698ffd73
Keith Kelly <keith@ices.utexas.edu>
2015-07-30 16:07:35 -0500

    Adds the -R_s flag (suggested number of sources when reduced, which is not respected if adaptive behavior is enabled)

--- scripts/run_faims.sh
+++ scripts/run_faims.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ../build/
-ibrun -np 1 ./faims -fmm_m 8 -fmm_q 10 -min_depth 3 -max_depth 3 -k 5 -R_d 25 -N_pts 10
+ibrun -np 1 ./faims -fmm_m 8 -fmm_q 10 -min_depth 3 -max_depth 3 -k 5 -R_d 25 -N_pts 10 -R_s 25
